891acde2585187d64145315f568bc1e7ee74c23e
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-12 22:41:30 +0000

    Update milestone_3.py

--- milestone_3.py
+++ milestone_3.py
@@ -1,15 +1,27 @@
-import random
+import random # importing the random module from the python library
 
-word_list = ['apple', 'banana', 'Strawberry', 'Orange', 'Pear']
-word = random.choice(word_list)
+word_list = ['Apple', 'Banana', 'Strawberry', 'Orange', 'Pear'] # list of fruit
+word = random.choice(word_list) # randomly chooses a fruit from the list
 
-while True:
-    guess = input("Please enter a single letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        if guess in word:
-            print(f"Good guess ,{guess}, is in the word.")
-            break
-        else:
-            print(f"Sorry, {guess} is not in the word")
-    print(word)
-    print('\n')
+def check_guess(guess): # defining the check_guess and taking the guess parameter as an argument
+    guess.lower()   # putting the users input as lower case
+    if len(guess) == 1 and guess.isalpha(): # this is a if statement where users input is 1 character length long and its an alphabetical input
+            if guess in word:   # another if statement where guess is in the word
+                print(f"Good guess ,{guess}, is in the word.")  # if it is then print this statement
+            else:                                               # else 
+                print(f"Sorry, {guess} is not in the word")     # if it isn't then print this statement
+    
+def ask_for_input():    # defining the ask_for_input function, where user input their guess for the word
+    check_guess(word)   # this calls the check_guess function and takes in the word variable
+    guess = input("Please enter a single letter: ") # this takes the users input and once entered it will take the users input into a while loop
+    while True: # if the while loop condition is True
+        if len(guess) == 1 and guess.isalpha(): # this is a if statement where users input is 1 character length long and its an alphabetical input
+            if guess in word:   # another if statement where guess is in the word
+                print("Good guess!")  # if it is then print this statement
+                break                 # break out of the loop
+            else:                     # else
+                print("Invalid letter. Please, enter a single alphabetical character.") # if it isn't then print this statement
+                print(word) # and print the word
+                break # break out of the loop
+
+ask_for_input() # calls the ask_for_input function
