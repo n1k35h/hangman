b8ec84f2b1d322caa952e8575b9520844a9b0700
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-14 22:00:00 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -2,7 +2,7 @@ import random # importing the random module from the python library
 
 class Hangman():    # hangman class
     def __init__(self, word_list, num_lives=5):  # setting up the initialiser and the attributes
-        self.word = word = random.choice(word_list)
+        self.word = word = random.choice(set(word_list))
         self.word_guessed = ["_"] * len(set(word))  # '_' = underscore will display depending on the length of the word 
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
