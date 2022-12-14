06d159128f3b7b975b74d9470693d59e93f1def3
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-14 22:19:00 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -5,7 +5,7 @@ word_list = ['Apple', 'Banana', 'Strawberry', 'Orange', 'Pear'] # list of fruit
 class Hangman():    # hangman class
     def __init__(self, word_list, num_lives=5):  # setting up the initialiser and the attributes
         self.word = word = random.choice(word_list)
-        self.word_guessed = ['_','_','_','_','_'] * len(set(word))  # '_' = underscore will display depending on the length of the word 
+        self.word_guessed = ['_'] * len(self.word)  # '_' = underscore will display depending on the length of the word 
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.word_list = word_list
