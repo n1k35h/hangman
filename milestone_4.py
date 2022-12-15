dd0142c97c26f22fa2318a4307a1033cc8b0468d
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-15 00:15:34 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -5,7 +5,7 @@ word = random.choice(word_list)
 
 class Hangman():    # hangman class
     def __init__(self, word_list, num_lives=5):  # setting up the initialiser and the attributes
-        self.word = word = random.choice(word_list)
+        self.word = word
         self.word_guessed = ['_'] * len(self.word)  # '_' = underscore will display depending on the length of the word 
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
@@ -14,11 +14,13 @@ class Hangman():    # hangman class
 
     def check_guess(self, guess): # defining the check_guess and taking the guess parameter as an argument
         guess.lower()   # putting the users input as lower case
-        if guess in word:   # another if statement where guess is in the word
+        if len(guess) == 1 and guess.isalpha(): # this is a if statement where users input is 1 character length long and its an alphabetical input
+            if guess in word:   # another if statement where guess is in the word
                 print(f"Good guess ,{guess}, is in the word.")  # if it is then print this statement
     
     def ask_for_input(self):    # defining the ask_for_input function, where user input their guess for the word
         guess = input("Please enter a single letter: ") # this takes the users input and once entered it will take the users input into a while loop
+        
         while True: # if the while loop condition is True
             if len(guess) != 1 and guess.isalpha(): # this is a if statement where users input is 1 character length long and its an alphabetical input
                 if guess not in word:   # another if statement where guess is in the word
@@ -29,7 +31,7 @@ class Hangman():    # hangman class
 
                 else:
                     self.check_guess(guess)   # this calls the check_guess function and takes in the word variable
-                    self.list_of_guesses.append(guess)
+                    self.list_of_guesses += guess
 
-game = Hangman(word_list = word, num_lives=5)
+game = Hangman(word_list, num_lives=5)
 game.ask_for_input()
