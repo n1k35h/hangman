bbbae9224fa132a4a2271dc49927ad752d131a40
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-15 23:04:24 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -8,5 +8,24 @@ class Hangman():
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
         self.list_of_guesses = []
-        
+
+    def check_guess(self, guess):
+        guess.lower()
+        if guess in self.word:
+            print(f"Good guess, {guess} is in the word.")
+        else:
+            print(f"{guess} is not in the word. Please try again!")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter a single letter: ")
+            if guess not in self.word:
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter! Please choose another letter.")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+
 game = Hangman(word_list=['apple', 'banana', 'orange', 'strawberry', 'blueberry'], num_lives=5)
+game.ask_for_input()
