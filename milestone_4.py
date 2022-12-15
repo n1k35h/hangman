786471694fef0b1737b06a9cbccd09204160871b
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-15 00:17:12 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -5,7 +5,7 @@ word = random.choice(word_list)
 
 class Hangman():    # hangman class
     def __init__(self, word_list, num_lives=5):  # setting up the initialiser and the attributes
-        self.word = word
+        self.word = word = random.choice(word_list)
         self.word_guessed = ['_'] * len(self.word)  # '_' = underscore will display depending on the length of the word 
         self.num_letters = len(set(self.word))
         self.num_lives = num_lives
