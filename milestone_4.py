0de2b7fb2ead02745193c2ebf0a8c28213e26dd4
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-16 21:59:16 +0000

    Update milestone_4.py

--- milestone_4.py
+++ milestone_4.py
@@ -14,6 +14,11 @@ class Hangman():
         if len(guess) == 1 and guess.isalpha():
             if guess in self.word:
                 print(f"Good guess! {guess} is in the word.")
+                for i in range(len(self.word)):
+                    if guess == self.word[i]:
+                        self.word_guessed[i] = guess
+                        
+                self.num_letters -= 1
 
     def ask_for_input(self):
         while True:
