d302587092993a5ab44a1c2399ca5e3882380510
n1k35h <119499198+n1k35h@users.noreply.github.com>
2022-12-20 19:59:02 +0000

    Create milestone_5.py

---
+++ milestone_5.py
@@ -0,0 +1,54 @@
+import random
+
+class Hangman():
+    def __init__(self, word_list, num_lives=5):
+        self.word_list = word_list
+        self.word = random.choice(word_list)
+        self.word_guessed = ['_'] * len(self.word)
+        self.num_letters = len(set(self.word))
+        self.num_lives = num_lives
+        self.list_of_guesses = []
+
+    def check_guess(self, guess):
+        guess.lower()
+        if len(guess) == 1 and guess.isalpha():
+            if guess in self.word:
+                print(f"Good guess! {guess} is in the word.")
+                for i in range(len(self.word)):
+                    if guess == self.word[i]:
+                        self.word_guessed[i] = guess
+
+                self.num_letters -= 1
+
+            else:
+                self.num_lives -= 1
+                print(f"Sorry, {guess} is not in the word.")
+                print(f"You have {self.num_lives} lives left.")
+
+    def ask_for_input(self):
+        while True:
+            guess = input("Please enter a single letter: ")
+            if len(guess) != 1 and not guess.isalpha():
+                print("Invalid letter. Please, enter a single alphabetical character.")
+            elif guess in self.list_of_guesses:
+                print("You already tried that letter!")
+            else:
+                self.check_guess(guess)
+                self.list_of_guesses.append(guess)
+                break
+
+def play_game(self, word_list):
+    game = Hangman
+    game(word_list, num_lives=5)
+    while True:
+        if self.num_lives == 0:
+            print("You lost!")
+        elif self.num_letters > 0:
+            self.ask_for_input()
+            continue
+        elif self.num_lives != 0 and not self.num_letters > 0:
+            print("Congratulations. You won the game!")
+        else:
+            break
+
+    self.play_game(word_list=['apple', 'banana', 'orange', 'strawberry', 'blueberry'])
